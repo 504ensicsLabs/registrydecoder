8ef7a37412a36f79523e457b34ab1ba38a6dde1d
atcuno <atcuno@gmail.com>
2011-09-06 16:04:18 +0000

    fixed init.py

--- __init__.py
+++ __init__.py
@@ -22,4 +22,4 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 #
-__all__= ["acquirers", "common", "GUI", "registryparser", "strings", "templates", "tree", "values", "visual", "datastructures", "initial_processing", "reporting", "errorclasses"]
+__all__= ["acquirers", "common", "GUI", "registryparser", "strings", "templates", "tree", "values", "datastructures", "initial_processing", "reporting", "errorclasses"]
