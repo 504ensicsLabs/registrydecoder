e44e98a26f602359821c37ff2fd543f95b49eeed
atcuno <atcuno@gmail.com>
2011-10-13 00:51:59 +0000

    add in kevin moores user assisst plugin, fix windows 7 usrclass.dat in regsitry_sigs

--- templates/template_files/user_assist.py
+++ templates/template_files/user_assist.py
@@ -1,12 +1,8 @@
-#
 # Registry Decoder
-# Copyright (c) 2011 Digital Forensics Solutions, LLC
-#
 # Contact email:  registrydecoder@digitalforensicssolutions.com
 #
 # Authors:
-# Andrew Case       - andrew@digitalforensicssolutions.com
-# Lodovico Marziale - vico@digitalforensicssolutions.com
+# Kevin Moore - kevinm@cert.org 
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,16 +18,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 #
-# TODO: Test me on XP
-#
-#
-
-import struct
 
 
 pluginname = "User Assist"
 description = ""
-hive = "NTUSER"    
+hives = ["NTUSER", "USRCLASS"]
 documentation = ""
 
 
@@ -39,29 +30,57 @@ ver_7_2008_ids = ("CEBFF5CD-ACE2-4F4F-9178-9926F41749EA", "F4E57C4B-2036-45F0-A9
     
 
 def run_me():
+    import struct
+    from datetime import datetime
+    
+    def convert_win_to_unix(windate):
+        no_nano = windate/10000000 # 10000000 - 100 nanosecond intervals in windows timestamp, remove them to get to seconds since windows epoch
+        unix = no_nano - 11644473600 # number of seconds between 1/1/1601 and 1/1/1970
+        return unix
 
+    date_format = '%m/%d/%Y %H:%M:%S'
     regkey = reg_get_required_key("\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist")
     subkeys = reg_get_subkeys(regkey)
     for key in subkeys:
         key_name = reg_get_key_name(key)
+        str_key_name = key_name[1:-1]
         key_path = "\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist"
         key_path += "\%s\count" % key_name
         skey = reg_get_required_key(key_path)
         values = reg_get_values(skey)
-        reg_report(("ID:", key_name)) 
+        reg_report(("ID:", key_name))
         for val in values:
             name = rot13(reg_get_value_name(val))
-            data = reg_get_value_data(val)
+            data = reg_get_raw_value_data(val)
 
-            if key_name in ver_7_2008_ids:
-                reg_report((name, data))
+            if str_key_name in ver_7_2008_ids:
+                y = struct.unpack("I", data[4:8])
+                z = struct.unpack("Q", data[60:68])
+                if z[0] > 0:
+                        dt = convert_win_to_unix(z[0])
+                        if dt >= 0:
+                            reg_report((name, str(y[0]), datetime.fromtimestamp(int(dt)).strftime(date_format)))
+                        else:
+                            reg_report((name, str(y[0])))
+                else:
+                    if y[0] != 0:
+                        reg_report((name, str(y[0])))
+                    else:
+                        reg_report((name))
 
             else: # pre_7_2008
                 if len(data) == 16:
-                    x, y, z, w = struct.unpack("IIII", data) 
-                    reg_report((name, str(x), str(y), str(z), str(w)))
+                    x, y, z = struct.unpack("IIQ", data)
+                    if z > 0:
+                        dt = convert_win_to_unix(z)
+                        if dt >= 0:         
+                            reg_report((name, str(x), str(y-5), datetime.fromtimestamp(int(dt)).strftime(date_format))) # ROT13 name, Session ID, RunCount, Last Run Date
+                        else:
+                            reg_report((name), str(x), str(y-5)) # UserAssist Values with invalid last run date
+                    else:
+                        reg_report((name)) # UserAssist Values with blank last run date
                 else:
-                    reg_report((name, data))
+                    reg_report((name))# UserAssist Values CTLCUA and CTLSESSION
 
         report((""))
 
